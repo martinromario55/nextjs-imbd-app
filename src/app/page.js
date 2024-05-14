0c3bafa1504585575d2950f8dafc93d73d9e7cd0
martinromario55 <martinromario55@gmail.com>
2024-05-14 12:01:26 +0300

    Fetch data from TMDB API

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import Results from '@/components/Results'
 
-const Home = () => {
+const API_KEY = process.env.API_KEY
+
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || 'fetchTrending'
+  // tmdb api
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  )
+  const data = await response.json()
+  if (!response.ok) {
+    console.log(error)
+    throw new Error('Something went wrong while fetching movies')
+  }
+  // console.log(data)
+  const results = data.results
   return (
     <div>
-      <h1 className="text-2xl font-extrabold">Home</h1>
+      <Results results={results} />
     </div>
   )
 }
