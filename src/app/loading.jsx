4f5b64ddbcb38090c1d67ac60832eca24fecfc48
martinromario55 <martinromario55@gmail.com>
2024-05-14 12:56:59 +0300

    Loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex justify-center mt-16">
+      <img className="h-52" src={'spinner.svg'} alt="loading..." />
+    </div>
+  )
+}
+
+export default loading
