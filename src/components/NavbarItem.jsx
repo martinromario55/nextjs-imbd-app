e20aed5c079e30ef788d65dd24d348618ecf3613
martinromario55 <martinromario55@gmail.com>
2024-05-14 15:18:03 +0300

    Suspense

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,24 +1,26 @@
 'use client'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const NavbarItem = ({ title, param }) => {
   const searchParams = useSearchParams()
   const genre = searchParams.get('genre')
   return (
-    <div>
-      <Link
-        href={`/?genre=${param}`}
-        className={`hover:text-amber-600 font-semibold ${
-          genre === param
-            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-            : ''
-        }`}
-      >
-        {title}
-      </Link>
-    </div>
+    <Suspense>
+      <div>
+        <Link
+          href={`/?genre=${param}`}
+          className={`hover:text-amber-600 font-semibold ${
+            genre === param
+              ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+              : ''
+          }`}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
   )
 }
 
